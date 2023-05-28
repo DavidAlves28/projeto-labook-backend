cf1a92c1795e889b02b071e95dd397b88202bf4c
david alves <dav.alvesc28@gmail.com>
2023-05-28 15:17:27 -0300

    configuracao  uuid e jwt

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import { postRoute} from "./router/userRoute";
+import dotenv from 'dotenv'
 
+dotenv.config()
 
 const app = express();
 
@@ -9,7 +11,7 @@ app.use(cors());
 
 app.use(express.json());
 
-app.listen(3003,()=>{console.log(`Servidor rodando na porta ${3003}`);
+app.listen(Number(process.env.PORT) || 3003,()=>{console.log(`Servidor rodando na porta ${3003}`);
 
 })
 app.use('/posts', postRoute)
